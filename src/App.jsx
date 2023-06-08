0a36fa5f1ead7e80c4c183de53ba7658c9066e6a
Vincent Enriquez <enriquezroseller23@gmail.com>
2023-06-08 20:36:10 +0800

    Add and remove local storage for logged-in state persistence

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Login from "./Components/Login/Login";
 import Home from "./Components/Home/Home";
 import MainHeader from "./Components/MainHeader/MainHeader";
@@ -6,12 +6,22 @@ import MainHeader from "./Components/MainHeader/MainHeader";
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const userLoggedInfo = localStorage.getItem("logged in");
+
+    if (userLoggedInfo === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = () => {
+    localStorage.setItem("logged in", "1");
     setIsLoggedIn(true);
     console.log("in app js");
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("logged in");
     setIsLoggedIn(false);
   };
   return (
