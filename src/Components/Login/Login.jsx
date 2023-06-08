f731f36f8a8e6a78f91fb1bb54c159fb475218dc
Vincent Enriquez <enriquezroseller23@gmail.com>
2023-06-08 12:09:43 +0800

    adding value and validations in email and password.

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,24 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Login.module.css";
 
 import Button from "../UI/Button/Button";
 import Card from "../UI/Card/Card";
 
 const Login = () => {
+  const [endteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [formIsValid, setFromIsValid] = useState(false);
+
+  const emailChangeHandler = (e) => {
+    setEnteredEmail(e.target.value);
+
+    setFromIsValid(
+      e.target.value.includes("") && enteredPassword.trim().length > 6
+    );
+  };
+
+  const passwordChangeHandler = (e) => {
+    setEnteredPassword(e.target.value);
+
+    setFromIsValid(
+      e.target.value.trim().length > 6 && endteredEmail.includes("@")
+    );
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault;
+  };
   return (
     <>
       <Card className={classes.login}>
-        <form>
+        <form onSubmit={submitHandler}>
           <div className={classes.control}>
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" />
+            <input
+              type="email"
+              id="email"
+              value={endteredEmail}
+              onChange={emailChangeHandler}
+            />
           </div>
           <div className={classes.control}>
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input
+              type="password"
+              id="password"
+              value={enteredPassword}
+              onChange={passwordChangeHandler}
+            />
           </div>
           <div className={classes.actions}>
-            <Button type="submit" className={classes.btn}>
+            <Button
+              type="submit"
+              className={classes.btn}
+              disabled={!formIsValid}>
               Log In
             </Button>
           </div>
