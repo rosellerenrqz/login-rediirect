2c5349d74ab01ec37bca5af381679a0b50bce3d4
Vincent Enriquez <enriquezroseller23@gmail.com>
2023-06-09 16:54:24 +0800

    updating auth context by using use context hook for a cleaner and nicer look of the code.

--- src/Components/MainHeader/Navigation.jsx
+++ src/Components/MainHeader/Navigation.jsx
@@ -1,35 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import Button from "../UI/Button/Button";
 import classes from "./Navigation.module.css";
 import AuthContext from "../../Auth/AuthContext";
 
 const Navigation = (props) => {
+  const context = useContext(AuthContext);
   return (
-    <AuthContext.Consumer>
-      {(context) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {context.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {context.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {context.isLoggedIn && (
-                <li>
-                  <Button onClick={props.onLogout}>Logout</Button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {context.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {context.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {context.isLoggedIn && (
+          <li>
+            <Button onClick={props.onLogout}>Logout</Button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
