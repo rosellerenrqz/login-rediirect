a344cf2c0598143ee2bf7a974d8983dbf6e37a98
Vincent Enriquez <enriquezroseller23@gmail.com>
2023-06-09 20:48:20 +0800

    adding logout button on home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import Card from "../UI/Card/Card";
+import Button from "../UI/Button/Button";
 import classes from "./Home.module.css";
+import AuthContext from "../../Auth/AuthContext";
 
 const Home = () => {
+  const context = useContext(AuthContext);
   return (
     <Card className={classes.home}>
       <h1>Welcome Back!</h1>
+      <Button onClick={context.onLogout}>Logout</Button>
     </Card>
   );
 };
